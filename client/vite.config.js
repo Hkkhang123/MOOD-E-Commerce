31b5d73bdd3c212fda4d8e3686cbd9f6024f8a72
kienkhang123 <kienkhanghoang123@gmail.com>
2025-03-27 19:06:31 +0700

    trang chu

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), ],
-  base: "/MOOD-E-Commerce"
+  base: "/MOOD-E-Commerce/"
 });
